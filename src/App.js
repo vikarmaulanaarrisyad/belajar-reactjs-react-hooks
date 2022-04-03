4b4add79e77d5850dc8d97b89864dfe87afac938
vikarmaulanaarrisyad <vikar.maulana.arrisyad@gmail.com>
2022-04-03 16:33:21 +0700

    menambahkan arrow function dan button click

--- src/App.js
+++ src/App.js
@@ -7,17 +7,27 @@ function App() {
   const age = 50;
   const link = "https:vikarmaulana.blogspot.com";
 
+  // Membuat Arrow Function
+  const clickMe = (name) => {
+    console.log('Hello, Saya ' + name);
+  }
+
   return (
     <div>
       {/* Component Header */}
       <Header />
-      
+
       <h1>
         {title}
       </h1>
       <h1>{ age * 3 }</h1>
       <a href={link}>Vikar Maulana</a>
+
+      
+    // Menambah clik event
+    <button onClick={ () => clickMe('Vikar Maulana Arrisyad') }>Click Me</button>
     </div>
+
   );
 }
 
